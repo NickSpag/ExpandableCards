d9f9f26b2b3c900edd49bdb51e2975b2aa8c1f2c
Nick Spagnola <spagnola8@gmail.com>
2019-06-12 14:52:19 -0700

    initial

---
+++ source/ExpandableCardExample/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExpandableCardExample.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
